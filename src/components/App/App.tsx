ce2ddf79eedd1c8e4ed9be91bae78bd1037115d6
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2023-11-02 15:58:01 +0600

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+      <div><button type="button" className="btn btn-primary">Primary</button></div>
+  )
+}
+
+export default App
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+type Ingredient = {
+  name: string,
+  price: number,
+  image: File
+}
